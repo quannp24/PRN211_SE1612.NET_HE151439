3633e4ff065a69522fae8e3e2ba4775a6c3c65f8
Quan Nguyen <bansiki114@gmail.com>
2022-07-19 18:43:03 +0700

    up lab3

---
+++ Hands-on-Lab/AutomobileSolutionLab3/AutomobileLibrary/Repository/CarRepository.cs
@@ -0,0 +1,24 @@
+﻿using AutomibileLibrary.DataAccess;
+using AutomobileLibrary.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileLibrary.Repository
+{
+    public class CarRepositorycs : ICarRepository
+    {
+        public void DeleteCar(int carId) => CarDAO.Instance.Remove(carId);
+
+        public Car GetCarById(int carId) => CarDAO.Instance.GetCarByID(carId);
+
+        public IEnumerable<Car> GetCars() => CarDAO.Instance.GetCarList();
+
+        public void InsertCar(Car car) => CarDAO.Instance.AddNew(car);
+
+        public void UpdateCar(Car car) => CarDAO.Instance.Update(car);
+
+    }
+}
